d7421c9878c58ac7c699d8df58efdeeb97d25b4c
gaju <gaju.mca@gmail.com>
2014-01-20 04:12:29 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cftask::Application.config.secret_key_base = '9e6f51cfefb4719a21938286e13d00e2e8841845160a69c4a0a3165a87a700194ffdc96df1221449e4f481805b84c5b0b1774d15158876151dce1aa387ce975e'
